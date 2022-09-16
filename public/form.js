f2b0236d067c9050bf015a491ed5841b18696784
NeilMcDeloitte <neilmclean@deloitte.co.uk>
2022-09-16 13:52:40 +0100

    Demo server-side and client-side validation

--- public/form.js
+++ public/form.js
@@ -1,20 +1,30 @@
-document.querySelector('[data-form]').addEventListener('submit', async(event)=> {
-  event.preventDefault();
+let validationMethod = 'server';
 
-  const formData = new FormData(event.target);
-  const formValues = Object.fromEntries(formData.entries());
-
-  await fetch('/form-cs', {
-    method: 'post',
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify(formValues),
-  }).then((response) => {
-    if (response.redirected) {
-      window.location.href = response.url;
-    } else {
-      response.json().then(data => {
-        console.log(data.errors);
-      });
-    }
+document.querySelectorAll('[data-validate]').forEach((item) => {
+  item.addEventListener('click', (event) => {
+    validationMethod = event.target.getAttribute('data-validate');
   });
+});
+
+document.querySelector('[data-form]').addEventListener('submit', async(event)=> {
+  if (validationMethod === 'client') {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const formValues = Object.fromEntries(formData.entries());
+
+    await fetch('/form-cs', {
+      method: 'post',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(formValues),
+    }).then((response) => {
+      if (response.redirected) {
+        window.location.href = response.url;
+      } else {
+        response.json().then(data => {
+          console.log(data.errors);
+        });
+      }
+    });
+  }
 });
